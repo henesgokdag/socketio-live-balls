8720db2b902106996fecba13455168a1f613f0fd
henesgokdag <henesgokdag@gmail.com>
2019-11-09 21:54:41 +0300

    users connect disconnect

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -2,17 +2,50 @@ app.controller("indexController", [
   "$scope",
   "indexFactory",
   ($scope, indexFactory) => {
-    const connectionOptions = {
-      reconnectionAttempts: 3,
-      reconnectionDelay: 600
+    $scope.init = () => {
+      const username = prompt("please enter username");
+      if (username) initSocket(username);
+      else return false;
     };
-    indexFactory
-      .connectSocket("http://localhost:3000", { connectionOptions })
-      .then(socket => {
-        console.log("bağlantı gerçekleşti", socket);
-      })
-      .catch(err => {
-        console.log(err);
-      });
+    $scope.messages = [
+     
+    ];
+    function initSocket(username) {
+      const connectionOptions = {
+        reconnectionAttempts: 3,
+        reconnectionDelay: 600
+      };
+      indexFactory
+        .connectSocket("http://localhost:3000", { connectionOptions })
+        .then((socket) => {
+		  socket.emit("newUser", { username });
+		  socket.on('newUser',(data)=>{
+			  const messageData= {
+				  type:{
+					  code:0, //server or user message
+					message:1  //connect
+				  }, 
+				  username:data.username,
+
+			  };
+			  $scope.messages.push(messageData);
+			  $scope.$apply();
+		  });
+		  socket.on('disUser',(data)=>{
+			const messageData= {
+				type:{
+					code:0,
+				  message:0 //disconnect
+				}, 
+				username:data.username
+			};
+			$scope.messages.push(messageData);
+			$scope.$apply();
+		  });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
   }
 ]);
--- src/socketapi.js
+++ src/socketapi.js
@@ -3,7 +3,27 @@ const io = socketio();
 const socketApi = {};
 socketApi.io= io;
 
+const users = {};
+
 io.on('connection',(socket)=>{
     console.log('a user connected');
+    socket.on('newUser',(data)=>{
+        console.log(data);
+        const defaultData = {
+            id:socket.id,
+            position:{
+                x:0,
+                y:0
+            }           
+        }
+        const userData= Object.assign(data,defaultData);
+        users[socket.id]= userData;
+        socket.broadcast.emit('newUser',users[socket.id]);
+    });
+    socket.on('disconnect',()=>{
+        socket.broadcast.emit('disUser',users[socket.id]);
+        delete users[socket.id];
+        
+    });
 });
 module.exports=socketApi;
