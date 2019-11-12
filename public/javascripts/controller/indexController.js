099c6115893627bc160805edd72d1b3427f223c7
henesgokdag <henesgokdag@gmail.com>
2019-11-12 22:13:29 +0300

    player create and animations

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -7,9 +7,8 @@ app.controller("indexController", [
       if (username) initSocket(username);
       else return false;
     };
-    $scope.messages = [
-     
-    ];
+    $scope.messages = [];
+    $scope.players = {};
     function initSocket(username) {
       const connectionOptions = {
         reconnectionAttempts: 3,
@@ -17,31 +16,54 @@ app.controller("indexController", [
       };
       indexFactory
         .connectSocket("http://localhost:3000", { connectionOptions })
-        .then((socket) => {
-		  socket.emit("newUser", { username });
-		  socket.on('newUser',(data)=>{
-			  const messageData= {
-				  type:{
-					  code:0, //server or user message
-					message:1  //connect
-				  }, 
-				  username:data.username,
-
-			  };
-			  $scope.messages.push(messageData);
-			  $scope.$apply();
-		  });
-		  socket.on('disUser',(data)=>{
-			const messageData= {
-				type:{
-					code:0,
-				  message:0 //disconnect
-				}, 
-				username:data.username
-			};
-			$scope.messages.push(messageData);
-			$scope.$apply();
-		  });
+        .then(socket => {
+          socket.emit("newUser", { username });
+          socket.on("initPlayers", players => {
+            $scope.players = players;
+            $scope.$apply();
+            console.log(players);
+          });
+          socket.on("newUser", data => {
+            const messageData = {
+              type: {
+                code: 0, //server or user message
+                message: 1 //connect
+              },
+              username: data.username
+            };
+            $scope.messages.push(messageData);
+            $scope.players[data.id] = data;
+            $scope.$apply();
+          });
+          socket.on("disUser", data => {
+            const messageData = {
+              type: {
+                code: 0,
+                message: 0 //disconnect
+              },
+              username: data.username
+            };
+            $scope.messages.push(messageData);
+            delete $scope.players[data.id];
+            $scope.$apply();
+          });
+          socket.on('animate',data=>{
+            $("#" + data.socketId).animate({ left: data.x, top: data.y }, () => {
+              animate = false;
+            });
+          });
+          let animate = false;
+          $scope.onClickPlayer = $event => {
+            if (!animate) {
+              let x = $event.offsetX;
+              let y = $event.offsetY;
+              socket.emit("animate", { x, y });
+              animate = true;
+              $("#" + socket.id).animate({ left: x, top: y }, () => {
+                animate = false;
+              });
+            }
+          };
         })
         .catch(err => {
           console.log(err);
--- src/socketapi.js
+++ src/socketapi.js
@@ -1,29 +1,34 @@
-const socketio = require('socket.io');
+const socketio = require("socket.io");
 const io = socketio();
 const socketApi = {};
-socketApi.io= io;
+socketApi.io = io;
 
 const users = {};
 
-io.on('connection',(socket)=>{
-    console.log('a user connected');
-    socket.on('newUser',(data)=>{
-        console.log(data);
-        const defaultData = {
-            id:socket.id,
-            position:{
-                x:0,
-                y:0
-            }           
-        }
-        const userData= Object.assign(data,defaultData);
-        users[socket.id]= userData;
-        socket.broadcast.emit('newUser',users[socket.id]);
-    });
-    socket.on('disconnect',()=>{
-        socket.broadcast.emit('disUser',users[socket.id]);
-        delete users[socket.id];
-        
-    });
+io.on("connection", socket => {
+  console.log("a user connected");
+  socket.on("newUser", data => {
+    console.log(data);
+    const defaultData = {
+      id: socket.id,
+      position: {
+        x: 0,
+        y: 0
+      }
+    };
+    const userData = Object.assign(data, defaultData);
+    users[socket.id] = userData;
+    socket.broadcast.emit("newUser", users[socket.id]);
+    socket.emit("initPlayers", users);
+  });
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("disUser", users[socket.id]);
+    delete users[socket.id];
+  });
+  socket.on("animate", data => {
+    users[socket.id].position.x = data.x;
+    users[socket.id].position.y = data.y;
+    socket.broadcast.emit("animate", { socketId: socket.id, x: data.x, y: data.y });
+  });
 });
-module.exports=socketApi;
+module.exports = socketApi;
